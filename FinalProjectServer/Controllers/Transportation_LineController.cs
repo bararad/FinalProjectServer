d0e50dcbd6327c72d55520ce22104f66dc78b55d
bararad <bararad@gmail.com>
2024-05-01 19:13:12 +0300

    Update Transportation_LineController.cs

--- FinalProjectServer/Controllers/Transportation_LineController.cs
+++ FinalProjectServer/Controllers/Transportation_LineController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace final_proj.Controllers
 {
