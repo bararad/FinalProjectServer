abb949cebda1030f14a0d111ec01f7a90d849670
OrelNahmany <157584402+OrelNahmany@users.noreply.github.com>
2024-05-02 19:26:43 +0300

    Add student disabillitis from server

---
+++ FinalProjectServer/Controllers/Student_disabilities_typeController.cs
@@ -0,0 +1,43 @@
+﻿using final_proj.BL;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace final_proj.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Student_disabilities_typeController : ControllerBase
+    {
+        // GET: api/<Student_disabilities_typeController>
+        [HttpGet]
+        public IEnumerable<Student_disabilities_type> Get()
+        {
+            return Student_disabilities_type.Read();
+        }
+        // GET api/<Student_disabilities_typeController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<Student_disabilities_typeController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<Student_disabilities_typeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<Student_disabilities_typeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ FinalProjectServer/DAL/DBservicesStudent_disabilities_type.cs
@@ -0,0 +1,90 @@
+﻿using final_proj.BL;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace final_proj.DAL
+{
+    public class DBservicesStudent_disabilities_type
+    {
+        public DBservicesStudent_disabilities_type()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        public SqlConnection connect(String conString)
+        {
+            // Read the connection string from the configuration file
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json").Build();
+            string cStr = configuration.GetConnectionString("myProjDB");
+            SqlConnection con = new SqlConnection(cStr);
+            con.Open();
+            return con;
+        }
+
+
+        public List<Student_disabilities_type> GetStudent_disabilities_type()
+
+        {
+            SqlConnection con;
+            SqlCommand cmd;
+            List<Student_disabilities_type> disabilitiesTypeList = new List<Student_disabilities_type>();
+
+            try
+            {
+                con = connect("myProjDB");
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+
+            cmd = CreateCommandWithSPWithoutParametersEs("SPproj_GetStudentType", con);
+
+            try
+            {
+                SqlDataReader dataReader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+                while (dataReader.Read())
+                {
+                    Student_disabilities_type disabilitiesType = new Student_disabilities_type();
+                    disabilitiesType.TypeCode = dataReader["type_code"].ToString();
+                    disabilitiesType.TypeDescription = dataReader["type_description"].ToString();
+
+                    disabilitiesTypeList.Add(disabilitiesType);
+                }
+                return disabilitiesTypeList;
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+            finally
+            {
+                if (con != null)
+                {
+                    con.Close();
+                }
+            }
+        }
+        private SqlCommand CreateCommandWithSPWithoutParametersEs(String spName, SqlConnection con)
+        {
+
+            SqlCommand cmd = new SqlCommand(); // create the command object
+
+            cmd.Connection = con;              // assign the connection to the command object
+
+            cmd.CommandText = spName;      // can be Select, Insert, Update, Delete 
+
+            cmd.CommandTimeout = 10;           // Time to wait for the execution' The default is 30 seconds
+
+            cmd.CommandType = System.Data.CommandType.StoredProcedure; // the type of the command, can also be text
+
+            return cmd;
+        }
+
+
+    }
+}
