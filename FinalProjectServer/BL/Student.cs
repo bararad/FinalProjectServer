153c5f1fa0e7c3159b548d94790f8f3cf240f50e
bararad <bararad@gmail.com>
2024-05-07 20:22:25 +0300

    dbs of studet and class

---
+++ FinalProjectServer/BL/Student.cs
@@ -0,0 +1,63 @@
+﻿namespace final_proj.BL
+{
+    public class Student
+    {
+        private string stu_fullName;
+        private string stu_id;
+        private DateTime stu_dateofbirth;
+        private string stu_grade;
+        private string stu_school;
+        private DateTime stu_dateOfPlacement;
+        private string stu_disability;
+        private string stu_comments;
+
+        public Student() 
+        {
+        }
+        public Student(string stu_fullName, string stu_id, DateTime stu_dateofbirth, string stu_grade, string stu_school, DateTime stu_dateOfPlacement, string stu_disability, string stu_comments)
+        {
+            Stu_fullName = stu_fullName;
+            Stu_id = stu_id;
+            Stu_dateofbirth = stu_dateofbirth;
+            Stu_grade = stu_grade;
+            Stu_school = stu_school;
+            Stu_dateOfPlacement = stu_dateOfPlacement;
+            Stu_disability = stu_disability;
+            Stu_comments = stu_comments;
+        }
+
+        public string Stu_fullName { get => stu_fullName; set => stu_fullName = value; }
+        public string Stu_id { get => stu_id; set => stu_id = value; }
+        public DateTime Stu_dateofbirth { get => stu_dateofbirth; set => stu_dateofbirth = value; }
+        public string Stu_grade { get => stu_grade; set => stu_grade = value; }
+        public string Stu_school { get => stu_school; set => stu_school = value; }
+        public DateTime Stu_dateOfPlacement { get => stu_dateOfPlacement; set => stu_dateOfPlacement = value; }
+        public string Stu_disability { get => stu_disability; set => stu_disability = value; }
+        public string Stu_comments { get => stu_comments; set => stu_comments = value; }
+
+
+        public int Insert()
+        {
+            DBservicesStudent dbs = new DBservicesStudent();
+            return dbs.InsertStudent(this);
+        }
+
+        public static List<Student> Read()
+        {
+            DBservicesStudent dbs = new DBservicesStudent();
+            return dbs.GetStudent();
+        }
+
+        public int Delete(string dd)
+        {
+            DBservicesStudent dbs = new DBservicesStudent();
+            return dbs.DeleteStudent(dd);
+        }
+
+        public int Update()
+        {
+            DBservicesStudent dbs = new DBservicesStudent();
+            return dbs.UpdateStudent(this);
+        }
+    }
+}
---
+++ FinalProjectServer/DAL/DBservicesStudent.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+using System.Text;
+using final_proj;
+using final_proj.BL;
+using final_proj.Controllers;
+
+public class DBservicesStudent
+{
+    public DBservicesStudent()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    //--------------------------------------------------------------------------------------------------
+    // This method creates a connection to the database according to the connectionString name in the web.config 
+    //--------------------------------------------------------------------------------------------------
+    public SqlConnection connect(String conString)
+    {
+
+        // read the connection string from the configuration file
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json").Build();
+        string cStr = configuration.GetConnectionString("myProjDB");
+        SqlConnection con = new SqlConnection(cStr);
+        con.Open();
+        return con;
+    }
+
+
+    public int InsertStudent(Student student)
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+
+        try
+        {
+            con = connect("myProjDB");
+        }
+        catch (Exception ex)
+        {
+            throw (ex);
+        }
+
+        cmd = CreateStudentInsertCommandWithStoredProcedure("SPproj_InsertStudent", con, student);
+
+        try
+        {
+            int numEffected = cmd.ExecuteNonQuery();
+            return numEffected;
+        }
+        catch (Exception ex)
+        {
+            throw (ex);
+        }
+        finally
+        {
+            if (con != null)
+            {
+                con.Close();
+            }
+        }
+    }
+    public List<Student> GetStudent()
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        List<Student> studentList = new List<Student>();
+
+        try
+        {
+            con = connect("myProjDB");
+        }
+        catch (Exception ex)
+        {
+            throw (ex);
+        }
+
+        cmd = CreateCommandWithSPWithoutParametersStu("SPproj_GetStudent", con);
+
+        try
+        {
+            SqlDataReader dataReader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+            while (dataReader.Read())
+            {
+                Student student = new Student();
+                student.Stu_fullName = dataReader["stu_fullName"].ToString();
+                student.Stu_id = dataReader["stu_id"].ToString();
+                student.Stu_dateofbirth = Convert.ToDateTime(dataReader["stu_dateofbirth"]);
+                student.Stu_grade = dataReader["stu_grade"].ToString(); 
+                student.Stu_school = dataReader["stu_school"].ToString(); 
+                student.Stu_dateOfPlacement = Convert.ToDateTime(dataReader["Stu_dateOfPlacement"]);
+                student.Stu_disability =dataReader["stu_disability"].ToString();
+                student.Stu_comments = dataReader["stu_comments"].ToString();
+                studentList.Add(student);
+            }
+            return studentList;
+        }
+        catch (Exception ex)
+        {
+            throw (ex);
+        }
+        finally
+        {
+            if (con != null)
+            {
+                con.Close();
+            }
+        }
+    }
+    public int UpdateStudent(Student student)
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+
+        try
+        {
+            con = connect("myProjDB");
+        }
+        catch (Exception ex)
+        {
+            throw (ex);
+        }
+
+        cmd = UpdateStudentCommandWithStoredProcedure("SPproj_UpdateStudent", con, student);
+
+        try
+        {
+            int numEffected = cmd.ExecuteNonQuery();
+            return numEffected;
+        }
+        catch (Exception ex)
+        {
+            throw (ex);
+        }
+        finally
+        {
+            if (con != null)
+            {
+                con.Close();
+            }
+        }
+    }
+    private SqlCommand CreateCommandWithSPWithoutParametersStu(String spName, SqlConnection con)
+    {
+
+        SqlCommand cmd = new SqlCommand(); // create the command object
+
+        cmd.Connection = con;              // assign the connection to the command object
+
+        cmd.CommandText = spName;      // can be Select, Insert, Update, Delete 
+
+        cmd.CommandTimeout = 10;           // Time to wait for the execution' The default is 30 seconds
+
+        cmd.CommandType = System.Data.CommandType.StoredProcedure; // the type of the command, can also be text
+
+        return cmd;
+    }
+
+    private SqlCommand UpdateStudentCommandWithStoredProcedure(string spName, SqlConnection con, Student student)
+    {
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = con;
+        cmd.CommandText = spName;
+        cmd.CommandTimeout = 10;
+        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+        cmd.Parameters.AddWithValue("@stu_fullName", student.Stu_fullName);
+        cmd.Parameters.AddWithValue("@stu_id", student.Stu_id);
+        cmd.Parameters.AddWithValue("@stu_dateofbirth", student.Stu_dateofbirth);
+        cmd.Parameters.AddWithValue("@stu_grade", student.Stu_grade);
+        cmd.Parameters.AddWithValue("@stu_school", student.Stu_school);
+        cmd.Parameters.AddWithValue("@stu_dateOfPlacement", student.Stu_dateOfPlacement);
+        cmd.Parameters.AddWithValue("@stu_disability", student.Stu_disability);
+        cmd.Parameters.AddWithValue("@stu_comments", student.Stu_comments);
+        return cmd;
+    }
+
+    private SqlCommand CreateStudentInsertCommandWithStoredProcedure(string spName, SqlConnection con, Student student)
+    {
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = con;
+        cmd.CommandText = spName;
+        cmd.CommandTimeout = 10;
+        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+        cmd.Parameters.AddWithValue("@stu_fullName", student.Stu_fullName);
+        cmd.Parameters.AddWithValue("@stu_id", student.Stu_id);
+        cmd.Parameters.AddWithValue("@stu_dateofbirth", student.Stu_dateofbirth);
+        cmd.Parameters.AddWithValue("@stu_grade", student.Stu_grade);
+        cmd.Parameters.AddWithValue("@stu_school", student.Stu_school);
+        cmd.Parameters.AddWithValue("@stu_dateOfPlacement", student.Stu_dateOfPlacement);
+        cmd.Parameters.AddWithValue("@stu_disability", student.Stu_disability);
+        cmd.Parameters.AddWithValue("@stu_comments", student.Stu_comments);
+        return cmd;
+    }
+
+}
