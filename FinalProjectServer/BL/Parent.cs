62b81f1745d40a0a84ea5de6389c06489923fb26
eliorGur <eliorgor19@gmail.com>
2024-05-07 20:24:17 +0300

    parent class

---
+++ FinalProjectServer/BL/Parent.cs
@@ -0,0 +1,31 @@
+﻿namespace final_proj.BL
+{
+    public class Parent
+    {
+        private string stu_parentName;
+        private string stu_parentCell;
+        private string stu_parentCity;
+        private string stu_parentStreet;
+        private int stu_parentHomeNum;
+        private string stu_id;
+
+        public Parent(string stu_parentName, string stu_parentCell, string stu_parentCity, string stu_parentStreet, int stu_parentHomeNum, string stu_id)
+        {
+            Stu_parentName = stu_parentName;
+            Stu_parentCell = stu_parentCell;
+            Stu_parentCity = stu_parentCity;
+            Stu_parentStreet = stu_parentStreet;
+            Stu_parentHomeNum = stu_parentHomeNum;
+            Stu_id = stu_id;
+        }
+
+        public Parent() { }
+
+        public string Stu_parentName { get => stu_parentName; set => stu_parentName = value; }
+        public string Stu_parentCell { get => stu_parentCell; set => stu_parentCell = value; }
+        public string Stu_parentCity { get => stu_parentCity; set => stu_parentCity = value; }
+        public string Stu_parentStreet { get => stu_parentStreet; set => stu_parentStreet = value; }
+        public int Stu_parentHomeNum { get => stu_parentHomeNum; set => stu_parentHomeNum = value; }
+        public string Stu_id { get => stu_id; set => stu_id = value; }
+    }
+}
