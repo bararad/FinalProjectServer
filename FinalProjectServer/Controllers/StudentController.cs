96f7e03ff5e97c9a98e8c89c28600a9318c4d1ab
eliorGur <eliorgor19@gmail.com>
2024-05-08 11:33:10 +0300

    Update StudentController.cs

--- FinalProjectServer/Controllers/StudentController.cs
+++ FinalProjectServer/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using final_proj.BL;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,34 +11,38 @@ namespace final_proj.Controllers
     {
         // GET: api/<StudentController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Student> Get()
         {
-            return new string[] { "value1", "value2" };
+            return Student.Read();
         }
+
+        // POST api/<StudentController>
+        [HttpPost]
+        public int Post([FromBody] Student student)
+        {
+            return student.Insert();
+        }
+
+        // PUT api/<StudentController>/5
+        [HttpPut()]
+        public int Put([FromBody] Student student)
+        {
+            return student.Update();
+        }
+
+
+        // DELETE api/<StudentController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
 
         // GET api/<StudentController>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
-        }
-
-        // POST api/<StudentController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<StudentController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<StudentController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
